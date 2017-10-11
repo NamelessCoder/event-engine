7061d7daa925743ab04555677ca7e3bfe4c2380b
Claus Due <claus@namelesscoder.net>
2017-10-12 01:28:22 +0200

    [FEATURE] Add special ANY_EVENT_TYPE wildcard type
    Returning this constant's value as a "handled event type" from a handler results in this handler receiving every possible event type, and receive the events after other handlers which subscribe to specific events have handled them.

--- src/EventDispatcher.php
+++ src/EventDispatcher.php
@@ -20,7 +20,13 @@ class EventDispatcher implements EventDispatcherInterface
 
     public function addHandler(EventHandlerInterface $eventHandler)
     {
-        foreach ($eventHandler->getHandledEventTypes() as $handledEventType) {
+        $handledEventTypes = $eventHandler->getHandledEventTypes();
+        if (in_array(EventTypeInterface::ANY_EVENT_TYPE, $handledEventTypes)) {
+            // An event handler which handles any event listens for no particular events and
+            // thus won't receive events twice, and will receive events after specific handlers.
+            $handledEventTypes = [EventTypeInterface::ANY_EVENT_TYPE];
+        }
+        foreach ($handledEventTypes as $handledEventType) {
             if (!isset($this->handlers[$handledEventType])) {
                 $this->handlers[$handledEventType] = [$eventHandler];
             } elseif (!in_array($eventHandler, $this->handlers[$handledEventType])) {
@@ -32,6 +38,12 @@ class EventDispatcher implements EventDispatcherInterface
     public function dispatch(EventInterface $event, bool $allowStopPropagation = false): EventInterface
     {
         foreach ($this->handlers[$event->getType()->getName()] ?? [] as $eventHandler) {
+            $event = $eventHandler->handleEvent($event);
+            if ($allowStopPropagation && $event->isStopped()) {
+                return $event;
+            }
+        }
+        foreach ($this->handlers[EventTypeInterface::ANY_EVENT_TYPE] ?? [] as $eventHandler) {
             $event = $eventHandler->handleEvent($event);
             if ($allowStopPropagation && $event->isStopped()) {
                 return $event;
--- src/EventTypeInterface.php
+++ src/EventTypeInterface.php
@@ -4,6 +4,7 @@ namespace NamelessCoder\EventEngine;
 
 interface EventTypeInterface
 {
+    const ANY_EVENT_TYPE = 42;
     public static function getAllNames(): array;
     public static function cast(string $type): EventTypeInterface;
     public function __toString(): string;
